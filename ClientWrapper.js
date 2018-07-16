b5de1300bb215fac7e57e5267057ac79b540bfdc
Christian Vadalà <chr.vadala@gmail.com>
2018-07-16 19:20:51 +0200

    adds ClientWrapper

---
+++ ClientWrapper.js
@@ -0,0 +1,38 @@
+class ClientWrapper {
+  constructor (mqtt) {
+    this.mqtt = mqtt
+  }
+
+  subscribe (topics, options) {
+    if (!Array.isArray(topics)) throw new Error('Should provide array with topics')
+    return new Promise((resolve, reject) => {
+      const cb = (err, granted) => {
+        if (err) return reject(err)
+        resolve(granted)
+      }
+
+      if (options)
+        this.mqtt.subscribe(topics, options, cb)
+      else
+        this.mqtt.subscribe(topics, cb)
+    })
+  }
+
+  unsubscribe (topics) {
+    if (!Array.isArray(topics)) throw new Error('Should provide array with topics')
+    return new Promise((resolve, reject) => {
+      const cb = err => {
+        if (err) return reject(err)
+        resolve()
+      }
+
+      this.mqtt.unsubscribe(topics, cb)
+    })
+  }
+
+  on (eventName, listener) {
+    return this.mqtt.on(eventName, listener)
+  }
+}
+
+module.exports = ClientWrapper
---
+++ ClientWrapper.spec.js
@@ -0,0 +1,38 @@
+const ClientWrapper = require('./ClientWrapper')
+
+test('should wrap mqtt.js client', function () {
+  const client = {
+    subscribe: jest.fn().mockImplementation((topics, optionsOrCb, cb) => {
+
+      let qos, realCb
+      if (typeof optionsOrCb === 'function') {
+        qos = 0
+        realCb = optionsOrCb
+      } else {
+        qos = optionsOrCb.qos || 0
+        realCb = cb
+      }
+
+      let res = topics.map(t => ({topic: t, qos}))
+      realCb(undefined, res)
+    }),
+
+    unsubscribe: jest.fn().mockImplementation((topics, cb) => {
+      cb(undefined)
+    }),
+
+    on: jest.fn(),
+  }
+
+  const wrapper = new ClientWrapper(client)
+  const fn1 = jest.fn()
+  wrapper.on('message', fn1)
+  expect(client.on).toBeCalledWith('message', fn1)
+
+  expect(wrapper.subscribe(['test1'], {qos: 1})).resolves.toMatchObject([{topic: 'test1', qos: 1}])
+  expect(wrapper.subscribe(['test2'])).resolves.toMatchObject([{topic: 'test2', qos: 0}])
+  expect(wrapper.unsubscribe(['test3'])).resolves.toBe(undefined)
+
+  expect(() => wrapper.subscribe('test1')).toThrow()
+  expect(() => wrapper.unsubscribe('test1')).toThrow()
+})
